9fccd37d12bad6122100cd2012e7690df3dd58c2
szczuru <szczuru@gmail.com>
2026-04-11 14:49:21 +0200

    Update jailbreak.h

--- external/libjbc/jailbreak.h
+++ external/libjbc/jailbreak.h
@@ -1,26 +1,8 @@
 #pragma once
-#include <stdint.h>
-#include "defs.h"
 
-struct jbc_cred
-{
-    uid_t uid;
-    uid_t ruid;
-    uid_t svuid;
-    gid_t rgid;
-    gid_t svgid;
-    uintptr_t prison;
-    uintptr_t cdir;
-    uintptr_t rdir;
-    uintptr_t jdir;
-    uint64_t sceProcType;
-    uint64_t sonyCred;
-    uint64_t sceProcCap;
-};
+#ifdef __PS4__
 
+// Minimal sandbox escape for PS4 FW 9.00 (based on sleirsgoevy/ps4-libjbc)
+int jailbreak_escape(void);
 
-uintptr_t jbc_get_prison0(void);
-uintptr_t jbc_get_rootvnode(void);
-int jbc_get_cred(struct jbc_cred*);
-int jbc_jailbreak_cred(struct jbc_cred*);
-int jbc_set_cred(const struct jbc_cred*);
+#endif
