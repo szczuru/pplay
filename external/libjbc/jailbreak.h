031c8d735245e121e32d1d7f09860f3f30068c3f
szczuru <szczuru@gmail.com>
2026-04-12 10:16:26 +0200

    Update jailbreak.h

--- external/libjbc/jailbreak.h
+++ external/libjbc/jailbreak.h
@@ -1,8 +1,5 @@
 #pragma once
 
 #ifdef __PS4__
-
-int jbc_escape(void);           // główna funkcja escape
-int jbc_get_prison0(void);      // pomocnicze (opcjonalnie)
-
+int jbc_escape(void);
 #endif
