8c55c93c0a3e91a20cd0544a4e796eafa73a36d6
szczuru <szczuru@gmail.com>
2026-04-12 10:17:05 +0200

    Simplify kernelrw_init function implementation

--- external/libjbc/kernelrw.c
+++ external/libjbc/kernelrw.c
@@ -1,13 +1,4 @@
 #include "kernelrw.h"
-#include <stdio.h>
-
 #ifdef __PS4__
-
-int kernelrw_init(void)
-{
-    printf("[pplay] kernelrw initialized (stub for 9.00)\n");
-    // Na 9.00 z GoldHEN kernelrw jest już w większości przypadków dostępny
-    return 0;
-}
-
+int kernelrw_init(void) { return 0; }   // stub - wystarczy na 9.00
 #endif
